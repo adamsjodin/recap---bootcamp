9c1cbddf1ddca218beea955c0dd59e6dfeaa6d1f
adamsjodin <adamsjodin@hotmail.com>
2023-10-10 17:30:30 +0200

    added components + guess the number game

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-import { useState, useEffect } from 'react'
 import './App.css'
+import { useState } from 'react'
+import ButtonEventFunction from './components/ButtonEvent';
+import ClickCounterFunction from './components/ClickCounter';
+import InputDuplicationFunction from './components/InputDuplication';
+import CountDownTimerFunction from './components/CountDownTimer';
+import ColorPickerFunction from './components/ColorPicker';
+import GuessTheNumberFunction from './components/GuessTheNumber';
 
 function App() {
   const [buttonEvent, setButtonEvent] = useState('Hello World');
@@ -9,93 +15,30 @@ function App() {
   const [countDownTimer, setCountDownTimer] = useState(5);
   const [countDownMessage, setCountDownMessage] = useState('');
 
-  const [suggestedColor, setSuggestedColor] = useState([])
   const [colorPicker, setColorPicker] = useState('');
+  const [suggestedColor, setSuggestedColor] = useState([]);
 
-  const colorList = [
-    "aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond", "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgrey", "darkgreen", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkslategrey", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dimgrey", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "grey", "green", "greenyellow", "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgrey", "lightgreen", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightslategrey", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "rebeccapurple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "slategrey", "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellow", "yellowgreen"
-  ]
-
-  function buttonEventFunction() {
-    setButtonEvent(prevButtonEvent => {
-      return prevButtonEvent === 'Hello World' ? 'Hello Adam' : 'Hello World';
-    });
-  };
-
-  function ClickCounterFunction() {
-    setClickCounter(prevClickCounter => prevClickCounter + 1);
-  };
-
-  function textTypeFunction(e) {
-    setInputDuplication(e.target.value);
-  };
-
-  useEffect(() => {
-    const countDown = () => {
-      if (countDownTimer >= 1) {
-        setCountDownTimer(prevCountDownTimer => prevCountDownTimer - 1)
-      } else {
-        setCountDownMessage('Times up!')
-      };
-    };
-
-    const timer = setInterval(countDown, 1000);
-
-    return () => clearInterval(timer)
-  }, [countDownTimer]);
-  
-  
-  function changeColorFunction(e) {
-
-    const userInput = e.target.value.toLowerCase();
-    const filteredColors = colorList.filter(color =>
-      color.toLowerCase().includes(userInput)
-    );
-  
-    setColorPicker(userInput);
-    setSuggestedColor(filteredColors);
-
-  }
 
   return (
     <div className="App">
       <h1>BOOTCAMP</h1>
       <section className='events'>
-        <button onClick={buttonEventFunction}>{buttonEvent}</button>
-
-        <h2>{clickCounter}</h2>
-        <button onClick={ClickCounterFunction}>Click to count</button>
-
-        <input
-          type="text"
-          placeholder='Type here'
-          onChange={textTypeFunction}
-          value={inputDuplication}
+        <ButtonEventFunction buttonEvent={buttonEvent} setButtonEvent={setButtonEvent}/>
+        <ClickCounterFunction clickCounter={clickCounter} setClickCounter={setClickCounter}/>
+        <InputDuplicationFunction inputDuplication={inputDuplication} setInputDuplication={setInputDuplication}/>
+        {/* <CountDownTimerFunction 
+          countDownTimer={countDownTimer}
+          setCountDownTimer={setCountDownTimer}  
+          countDownMessage={countDownMessage}
+          setCountDownMessage={setCountDownMessage}
+        /> */}
+        <ColorPickerFunction 
+          colorPicker={colorPicker}
+          setColorPicker={setColorPicker}
+          suggestedColor={suggestedColor}
+          setSuggestedColor={setSuggestedColor}
         />
-        <h2>{inputDuplication}</h2>
-        <h2>{countDownTimer} seconds left</h2>
-        <p>{countDownMessage}</p>
-
-        <div style={{ backgroundColor: colorPicker, padding: 100 }}>
-          <input
-            style={{ padding: 10 }}
-            type="text"
-            value={colorPicker}
-            placeholder='Enter background color'
-            onChange={changeColorFunction}
-          />
-          <select value={colorPicker} onChange={changeColorFunction}>
-            {
-              suggestedColor.map((option, i) => (
-                <option key={i} value={option}>
-                  {option}
-                </option>
-
-              ))
-            }
-          </select>
-        </div>
-
+        <GuessTheNumberFunction guessedNumber={guessedNumber} setGuessedNumber={setGuessedNumber}/>
       </section>
     </div>
   )
